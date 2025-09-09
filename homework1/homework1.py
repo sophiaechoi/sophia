2aaede2648e890f8c5bc5c75a885d91f95ab736a
Sophia Choi <sophiachoi@berkeley.edu>
2025-09-08 17:09:44 -0700

    initial commit

---
+++ homework1/homework1.py
@@ -0,0 +1,27 @@
+# --- Variables and Data Types ---
+a = 10
+print (a)
+print(type(a))
+#a is an integer, a whole number with no decimals
+b = 1.5
+print (b)
+print(type(b))
+#b is a float, a number that includes decimals
+c = 3j
+print(c)
+print(type(c))
+#c is a complex number, where j equals to the square root of negative one
+d = "hello"
+print(d)
+print(type(d))
+#d is an str, or string, a series of characters that lie between quotation marks
+e = [1,2,3]
+print(e)
+print(type(e))
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f))
+g = (1,2)
+print(g)
+print(type(g))
+h = ["apple", "banana", "strawberry"]
